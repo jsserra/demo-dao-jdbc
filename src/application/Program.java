219ab3677d436b143624063e2b978460504baf91
Juliano Sales <jsserra21@gmail.com>
2019-01-25 16:52:34 -0200

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,11 +11,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
+		/*Department obj = new Department(1, "Books");
 		Seller seller = new Seller(1, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, obj);
-		System.out.println(seller);
+		System.out.println(seller);*/
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -11,8 +11,24 @@ import java.util.Properties;
 
 public class DB {
 
-	private static Connection conn = null;
 	
+	//Lendo, Estabelecendo e Fechando a conexão com os 3 métodos: loadProperties() getConnection() closeConection()
+	
+	private static Connection conn = null;
+
+	//1º Método para ler os dados de db.properties, armazenar em um objeto do tipo Properties e retorna-lo 
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs); //o método load -> faz a leitura do arquivo properties apontado pelo FileInputStream e vai guardar os dados dentro do objeto props
+			return props;
+		}
+		catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+	
+	//2º Método para estabelecer a conexão
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
@@ -27,6 +43,7 @@ public class DB {
 		return conn;
 	}
 	
+	//3º método para fechar a conexão
 	public static void closeConnection() {
 		if (conn != null) {
 			try {
@@ -37,16 +54,7 @@ public class DB {
 		}
 	}
 	
-	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("db.properties")) {
-			Properties props = new Properties();
-			props.load(fs);
-			return props;
-		}
-		catch (IOException e) {
-			throw new DbException(e.getMessage());
-		}
-	}
+
 	
 	public static void closeStatement(Statement st) {
 		if (st != null) {
